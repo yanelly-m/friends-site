02f4eb32e296c973f9061da77db3d9a667d06874
yanelly-m <68297772+yanelly-m@users.noreply.github.com>
2020-07-16 22:13:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-/*global createCanvas, background */
+/*global createCanvas, background, noStroke, fill, rect, text, textSize, mouseY, mouseX, ellipse*/
+
 function setup(){
-  createCanvas(900,250);
-  background(112, 103, 106);
+  createCanvas(800,500);
+  background(128,128,128);
+  textSize(32)
+  text("Click here for a fun trivia fact!", 190, 240);
+  //rect(x top L, y top L, width, height)
 }
 
 function mouseClicked(){
- if(mouseX > 200 && mouseX < 600 && mouseY > 200 && mouseY < 300){
-    background(112, 103, 106);
-    fill(0,255,0);
-    textSize(32);
-    text("Click for a fun fact", width/2-50, 100);
-    fill(255,0,0);
+  if(mouseX > 200 && mouseX < 600 && mouseY > 200 && mouseY < 300){
+    background(255,183,0);
+    fill(0);
+    textSize(20);
+    text("Click to Restart", 300, 150);
+    textSize(32)
+    text("Friends was originally called Insomnia Café", 90, 240)
   } else{
-    background(112, 103, 106);
+    background(128,128,128);
     noStroke();
-    fill(188, 188, 188);
-    rect(200,200,400,100);
+    fill(0)
+    textSize(32)
+    text("Click here for a fun trivia fact!", 190, 240);
   } 
 }
